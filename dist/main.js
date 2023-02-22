fb04e82bbece1462493f17d5f69390eed0d079a4
Kevin Okoth <kevinokotth04@gmail.com>
2023-02-22 22:10:39 +0300

    Add dist folder

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"Hello world\");\r\n\n\n//# sourceURL=webpack://todolist/./src/index.js?");
+eval("const myTasks = [\r\n  {\r\n    description: \"Build no profit projects\",\r\n    completed: false,\r\n    index: 0,\r\n  },\r\n  {\r\n    description: \"Nature walk\",\r\n    completed: false,\r\n    index: 1,\r\n  },\r\n  {\r\n    description: \"Learn new languages\",\r\n    completed: false,\r\n    index: 2,\r\n  },\r\n  {\r\n    description: \"Make pastries\",\r\n    completed: false,\r\n    index: 3,\r\n  },\r\n];\r\nclass TodoList {\r\n  static addMyTask(task) {\r\n    const wrapper = document.querySelector(\".wrapper\");\r\n\r\n    wrapper.innerHTML += `\r\n        <div class=\"todo-content\">\r\n          <div class=\"content-list\">\r\n            <input type=\"checkbox\" name=\"checkbox\" id=\"checkbox\" />\r\n            <p class=\"todo-title\">${task.description}</p>\r\n            <div class=\"btn-container\">\r\n              <button class=\"edit\"><i class=\"uil uil-ellipsis-v\"></i></button>\r\n              <button class=\"delete hide\">\r\n                <i class=\"uil uil-trash-alt\"></i>\r\n              </button>\r\n            </div>\r\n          </div>\r\n          </div><hr> \r\n    `;\r\n  }\r\n}\r\nfor (let i = 0; i < myTasks.length; i += 1) {\r\n  TodoList.addMyTask(myTasks[i]);\r\n}\r\n\n\n//# sourceURL=webpack://todolist/./src/index.js?");
 
 /***/ })
 
