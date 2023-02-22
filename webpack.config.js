40481b9f159e40ae0dbd1934ac8fd1424cd040da
Kevin Okoth <kevinokotth04@gmail.com>
2023-02-22 16:19:22 +0300

    Add webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
+};
