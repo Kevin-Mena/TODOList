9a4766058eb02aa14039cf66b4429bbce44ffe72
Kevin Okoth <kevinokotth04@gmail.com>
2023-03-02 17:31:07 +0300

    Fix errors

--- index.test.js
+++ index.test.js
@@ -2,45 +2,45 @@
  * @jest-environment jsdom
  */
 
-let element = document.createElement("div");
-let ul = document.createElement("ul");
-ul.setAttribute("id", "list");
+const element = document.createElement('div');
+const ul = document.createElement('ul');
+ul.setAttribute('id', 'list');
 element.appendChild(ul);
-let todoInput = document.createElement("input");
-todoInput.value = "Make videos";
+const todoInput = document.createElement('input');
+todoInput.value = 'Make videos';
 let todos = [];
-let todo = {
+const todo = {
   completed: false,
   description: String,
 };
 
 // ADD TODO
 const addTodo = () => {
-  const value = todoInput.value;
+  const { value } = todoInput;
   if (value) {
     todo.description = value;
     todos.push(todo);
     for (let i = 0; i < todos.length; i += 1) {
       todos[i].index = i + 1;
     }
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
   }
 };
 
 const deleteTodo = (id) => {
-  let filteredTodo = todos.filter((todo) => todo.index !== id);
+  const filteredTodo = todos.filter((todo) => todo.index !== id);
   todos = filteredTodo;
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
   return filteredTodo;
 };
 
-describe("Test add and delete", () => {
-  test("Add a todo", () => {
+describe('Test add and delete', () => {
+  test('Add a todo', () => {
     addTodo();
     expect(todos.length).toBe(1);
   });
 
-  test("Delete a todo", () => {
+  test('Delete a todo', () => {
     deleteTodo(1);
     expect(todos.length).toBe(0);
   });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import "./style.css";
+import './style.css';
 // SELECT REQUIRED ELEMENTS
-const todoInput = document.querySelector("#addlist");
-const todoForm = document.querySelector(".todo-form");
-const todoWrapper = document.querySelector(".wrapper");
-const deleteCompleted = document.querySelector(".remove");
+const todoInput = document.querySelector('#addlist');
+const todoForm = document.querySelector('.todo-form');
+const todoWrapper = document.querySelector('.wrapper');
+const deleteCompleted = document.querySelector('.remove');
 
 const addLocalStorage = (todo) => {
-  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
   todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
 };
 
 // ADD TODO
@@ -16,30 +16,30 @@ const addTodo = (e) => {
   e.preventDefault();
   const todo = todoInput.value;
   if (todo) {
-    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
     const id = todos.length + 1;
     const completed = false;
     addLocalStorage(id, todo, completed);
-    const todoItem = document.createElement("div");
+    const todoItem = document.createElement('div');
     todoItem.className = `todo-item${id}`;
-    todoItem.setAttribute("id", "new-todo");
+    todoItem.setAttribute('id', 'new-todo');
     todoItem.innerHTML = `<input type="checkbox" class="todo-check">
           <input type="text" class="todo-text" value="${todo}" disabled>
           <button class="edit">Edit</button>
           <button class="delete">Delete</button>`;
     todoWrapper.appendChild(todoItem);
-    todoInput.value = "";
+    todoInput.value = '';
   }
 };
 // REMOVE FROM LOCAL STORAGE
 const removeLocalStorage = (id) => {
-  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  let todos = JSON.parse(localStorage.getItem('todos')) || [];
   todos = todos.filter((todo) => todo.id !== id);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
 };
 // DELETE TODO
 const deleteTodo = (e) => {
-  if (e.target.classList.contains("delete")) {
+  if (e.target.classList.contains('delete')) {
     removeLocalStorage(e.target.parentElement.classList[1]);
     e.target.parentElement.remove();
   }
@@ -47,89 +47,87 @@ const deleteTodo = (e) => {
 
 // CHECK TODO
 const checkTodo = (e) => {
-  if (e.target.className === "todo-check") {
+  if (e.target.className === 'todo-check') {
     const id = e.target.parentElement.classList[1];
     const isChecked = e.target.checked;
 
     if (isChecked) {
-      e.target.nextElementSibling.style.textDecoration = "line-through";
+      e.target.nextElementSibling.style.textDecoration = 'line-through';
     } else {
-      e.target.nextElementSibling.style.textDecoration = "none";
+      e.target.nextElementSibling.style.textDecoration = 'none';
     }
-    let todos = JSON.parse(localStorage.getItem("todos")) || [];
+    let todos = JSON.parse(localStorage.getItem('todos')) || [];
     todos = todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = isChecked;
       }
       return todo;
     });
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
   }
 };
 
 // GET TODO
 const getTodo = () => {
-  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
   todos.forEach((todo) => {
-    const todoItem = document.createElement("div");
+    const todoItem = document.createElement('div');
     todoItem.className = `todo-item${todo.id}`;
     todoItem.innerHTML = `<input type="checkbox" class="todo-check" ${
-      todo.completed ? "checked" : ""
+      todo.completed ? 'checked' : ''
     }>
           <input type="text" class="todo-text ${
-            todo.completed ? "done" : ""
-          }" value="${todo}" disabled/>
+  todo.completed ? 'done' : ''
+}" value="${todo}" disabled/>
           <button class="edit">Edit</button>
           <button class="delete">Delete</button>`;
     todoWrapper.appendChild(todoItem);
-    todoInput.value = "";
+    todoInput.value = '';
   });
 };
 // REMOVE DONE TODOS
 const removeDone = (e) => {
-  if (e.target.classList.contains("remove")) {
-    let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  if (e.target.classList.contains('remove')) {
+    let todos = JSON.parse(localStorage.getItem('todos')) || [];
     todos = todos.filter((todo) => todo.checked === true);
-    localStorage.setItem("todos", JSON.stringify(todos));
-    todoWrapper.innerHTML = "";
+    localStorage.setItem('todos', JSON.stringify(todos));
+    todoWrapper.innerHTML = '';
     getTodo();
   }
 };
 
 // EDIT LOCAL STORAGE
 const editLocalStorage = (id) => {
-  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  let todos = JSON.parse(localStorage.getItem('todos')) || [];
   todos = todos.map((todo) => {
     if (todo.id === id) {
       todo.text = todo;
     }
     return todo;
   });
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
 };
 
 // EDIT TODO
 const editTodo = (e) => {
-  if (e.target.className === "edit") {
-    const todoText = e.target.parentElement.querySelector(".todo-text");
+  if (e.target.className === 'edit') {
+    const todoText = e.target.parentElement.querySelector('.todo-text');
     const id = e.target.parentElement.classList[1];
     if (todoText.disabled) {
       todoText.disabled = false;
-      e.target.textContent = "Save";
+      e.target.textContent = 'Save';
     } else {
       todoText.disabled = true;
-      e.target.textContent = "Edit";
+      e.target.textContent = 'Edit';
       editLocalStorage(id, todoText.value);
     }
   }
 };
 
 // EVENT LISTENER FOR TODO
-todoForm.addEventListener("submit", addTodo);
-todoWrapper.addEventListener("click", deleteTodo);
-todoWrapper.addEventListener("click", checkTodo);
-todoWrapper.addEventListener("click", editTodo);
-window.addEventListener("DOMContentloaded", getTodo);
-deleteCompleted.addEventListener("click", removeDone);
-
-(module.exports = addTodo), deleteTodo;
+todoForm.addEventListener('submit', addTodo);
+todoWrapper.addEventListener('click', deleteTodo);
+todoWrapper.addEventListener('click', checkTodo);
+todoWrapper.addEventListener('click', editTodo);
+window.addEventListener('DOMContentloaded', getTodo);
+deleteCompleted.addEventListener('click', removeDone);
